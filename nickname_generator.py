30196fbd75014da4318c509941bea74a748a7cae
bwainaina380 <bwainaina380@gmail.com>
2021-03-27 11:54:42 +0300

    Day 1 of 100 project - Created a nickname generator based on a person's dream nationality and favorite superhero character

---
+++ nickname_generator.py
@@ -0,0 +1,6 @@
+print("Hellooooo! Welcome to the nickname generator.\nJust complete the following prompts and get your new nickname in no time\n")
+
+dream_nationality = input("1. Which nationality would you like to be? (Example: Canadian, British, etc.) \n")
+super_hero = input("2. What is the name of your favorite superhero? (Example: Aquaman, Spiderman) \n")
+
+print(f"\nYour nickname could be '{dream_nationality} {super_hero}'")
