6684e14fd9bb78985670d9de8e01db0fd85926c4
bwainaina380 <bwainaina380@gmail.com>
2021-04-12 14:25:36 +0300

    feat: Added a question getter and setter method

--- Quiz/question.py
+++ Quiz/question.py
@@ -3,8 +3,14 @@ class Question:
         self.question = question
         self.answer = answer
 
+    def get_question(self):
+        return self.question
+
+    def get_answer(self):
+        return self.answer
+
     def is_answer_correct(self, answer):
-        if self.answer == answer:
+        if self.get_answer() == answer:
             return True
         else:
             return False
