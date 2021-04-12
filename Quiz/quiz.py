f27e03d9adbaa52ded18c5e4346a9dffb2c8b6a0
bwainaina380 <bwainaina380@gmail.com>
2021-04-12 14:27:01 +0300

    feat: Added the generate_questions method

--- Quiz/quiz.py
+++ Quiz/quiz.py
@@ -1,17 +1,24 @@
-class Quiz:
-    def __init__(self, score, questions):
-        self.score = score
-        self.questions = questions
+from question import Question
 
-    def show_question(self):
+class Quiz:
+    def __init__(self, score, questions_list):
+        self.score = score
+        self.questions_list = questions_list
+        self.questions = []
+
+    def generate_questions(self):
+        for question in self.questions_list:
+            self.questions.append(Question(question["question"], question["answer"]))
+
+    def start_quiz(self):
+        self.generate_questions()
         for question in self.questions:
-            answer = input(question["question"])
+            answer = input(question.get_question())
             self.score.update_score(question.is_answer_correct(answer))
             if question.is_answer_correct(answer) == False:
                 print("You got the wrong answer")
-                print(f"Your final score is {self.score.get_score()}")
+                print(f"Your final score is {self.score.get_score()}\n")
                 break
             else:
                 print("Correct")
-                print(f"Your current score is {self.score.get_score()}")
-        print(f"Congratulations! You got all answers. Your final score is {self.score.get_score()}")
+                print(f"Your current score is {self.score.get_score()}\n")
