751a053adcbf401d426d8b4115fdd3d33f17c7cd
bwainaina380 <bwainaina380@gmail.com>
2021-04-12 14:31:20 +0300

    feat: Created the questions list

---
+++ Quiz/questions_list.py
@@ -0,0 +1,10 @@
+questions = [
+    {
+        "question": "A crocodile is a reptile: True or False? ",
+        "answer": "true"
+    },
+    {
+        "question": "How old was Captain Sanders when he created KFC? ",
+        "answer": "65"
+    }
+]
