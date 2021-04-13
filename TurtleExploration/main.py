2c74eb0aa108d182ac9ba775262e96c76395a327
bwainaina380 <bwainaina380@gmail.com>
2021-04-13 21:49:35 +0300

    feat: Added functionality to change the turtle heading

--- TurtleExploration/main.py
+++ TurtleExploration/main.py
@@ -1,9 +1,18 @@
 from turtle import Turtle, Screen
 
-turtle_tom = Turtle()
-turtle_tom.shape("turtle")
-turtle_tom.color("orange")
-turtle_tom.forward(300)
+def create_turtle():
+    turtle_tom = Turtle()
+    turtle_tom.shape("turtle")
+    turtle_tom.color("orange")
+    return turtle_tom
+
+def draw_shapes(num_sides, turtle_instance):
+    for side in range(1, num_sides):
+        turtle_instance.forward(30)
+        angle = int(360/num_sides)
+        turtle_instance.setheading(angle)
+
+draw_shapes(10, create_turtle())
 
 screen = Screen()
 screen.exitonclick()
